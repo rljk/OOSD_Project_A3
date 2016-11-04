29c32c1b4c3b934e9739177ebb944a02b2fd49ac
spikesdphan <michael-phan@uiowa.edu>
2016-11-04 09:53:59 -0500

    just adding a bit of mock inventory

--- orderSubsystem.java
+++ orderSubsystem.java
@@ -3,9 +3,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class orderSubsytem {
+public class orderSubsystem {
 	
-	public orderSubsytem() {
+	List<Integer> mockInventory = new ArrayList<Integer>();
+	
+	
+	public orderSubsystem() {
 		// this should be the initializer 
 		// should we leave it blank 
 		// or have it have an item?
@@ -17,5 +20,17 @@ public class orderSubsytem {
 			// go do its thing.
 		List<Integer> order = new ArrayList<Integer>();
 		Queue<List> orderQueue = new LinkedList<List>();
+		mockInventory.add(1);
+		mockInventory.add(2);
+		mockInventory.add(3);
+		mockInventory.add(4);
 	}
+	
+	public boolean isItemPresent(Integer id) {
+		for (Integer item : mockInventory) {	// don't know how inventory will come out
+			if (id == item) { return true;}
+		}
+		return false;
+	}
+	
 }
