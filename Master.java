751c1b4366ca4db6512e498e2246a572e0d6e3d8
Wold <ianwold@uiowa.edu>
2016-11-04 10:01:13 -0500

    Added main to Master

--- Master.java
+++ Master.java
@@ -1,4 +1,34 @@
+import java.util.Timer;
+
 public class Master
 {
+	Inventory inventory;
+	Visualizer visualizer;
 	
+	public Visualizer GetVisualizer()
+	{
+		return visualizer;
+	}
+	
+	public Inventory GetInventory()
+	{
+		return Inventory;
+	}
+	
+	public static void main(String[] args)
+	{
+		inventory = new Inventory();
+		visualizer = new Visualizer();
+		
+		Timer timer = new Timer();
+		
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				//Call .tick() on everything - in what order?
+				//Call this last:
+				Visualizer.tick();
+			}
+		}, 0, 1000);
+	}
 }
