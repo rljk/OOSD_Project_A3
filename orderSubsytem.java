23a765a06b17fb1559cfc36950faf31af24de6e9
spikesdphan <michael-phan@uiowa.edu>
2016-11-03 20:35:14 -0500

    Adding a test class

---
+++ orderSubsytem.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class orderSubsytem {
+	
+	public orderSubsytem() {
+		// this should be the initializer 
+		// should we leave it blank 
+		// or have it have an item?
+		// or would that me an order method?
+			// order method can take a list
+			// or something to that effect and
+			// just do a if the item id is 
+			// in the inventory, tell the robot to 
+			// go do its thing.
+		List<Integer> order = new ArrayList<Integer>();
+		Queue<List> orderQueue = new LinkedList<List>();
+	}
+}
